46f6cb73921a13149487dc00119af33f0f913162
Anthony Xavier <anthony-xavier@live.com>
2021-08-19 12:36:05 +1000

    Fixes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { history } from './history';
+import history from './history';
 import { render } from 'react-dom';
 
 import App from './components/App';
